23cd6a08d5045354d23b203191986e2056db4818
Sertunc Selen <cahit.selen@nvi.gov.tr>
2024-02-27 16:16:00 +0300

    initial commit and add font and color

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ["Gemunu Libre", "sans-serif"],
+        open: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        "gega-red": "#BC1A45",
+        "gega-melon": "#FFD369",
+        "gega-grey": "#DDDDDD",
+        "gega-white": "#F7F7F7",
+      },
+    },
+  },
+  plugins: [],
+};
